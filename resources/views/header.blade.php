4a55cecf7ee7e6657dc574342deda0ea496f3a81
Israa Mohamed <saramohamed2315@gmail.com>
2025-05-18 15:14:25 +0300

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,7 +3,7 @@
 <header>
     <nav style="background-color: rgb(113, 172, 211); padding: 15px 30px; box-shadow: 0 2px 5px rgba(0,0,0,0.1); display: flex; justify-content: space-between; align-items: center; position: fixed; width: 100% ; top: 0;  box-sizing: border-box;">
         <div style="display: flex; align-items: center; gap: 30px; margin: 0;">
-            <img src="../../public/img/logo9.png" alt="Logo" style="height: 50px; width: 50px; border-radius: 50%; object-fit: cover;">
+            <img src= "{{asset ('/img/logo9.png') }}" alt="Logo" style="height: 50px; width: 50px; border-radius: 50%; object-fit: cover;">
             <p style="margin: 0; color: #FFFFFF; font-size: 24px; white-space: nowrap;">Registration Form</p>
         </div>
         <div style="display: flex; align-items: center; flex-wrap: wrap; max-width: 70%; justify-content: flex-end;">
