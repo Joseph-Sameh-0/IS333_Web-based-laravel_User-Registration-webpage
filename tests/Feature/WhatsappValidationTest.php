83e9d4c60a9a88a894346357bb5353e4581e3231
IsraaXx <saramohamed2315@gmail.com>
2025-06-01 22:14:41 +0300

    test missing number

--- tests/Feature/WhatsappValidationTest.php
+++ tests/Feature/WhatsappValidationTest.php
@@ -13,5 +13,11 @@ class WhatsAppValidationTest extends TestCase {
         $response->assertStatus(200);
     }
 
+    public function test_it_returns_error_for_missing_phone_number() : void {
+        $response = $this->postJson('/check-whatsapp', []);
+
+        $response->assertStatus(422); // laravel returns 422 for validation errors!
+        $response->assertJsonValidationErrors('phone_number');
+    }
 }
 
