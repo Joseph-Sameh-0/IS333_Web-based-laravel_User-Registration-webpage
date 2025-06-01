535a19ea3016eeaa93354e293a669e1a8ce23f8d
IsraaXx <saramohamed2315@gmail.com>
2025-06-01 22:08:35 +0300

    test valid Whatsapp

--- tests/Feature/WhatsappValidationTest.php
+++ tests/Feature/WhatsappValidationTest.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class WhatsAppValidationTest extends TestCase {
-    
+    public function test_return_valid_for_valid_whatsapp() : void {
+
+        $response = $this->postJson('/check-whatsapp',['phone_number'=> '+201205563356']);
+        $response->assertStatus(200);
+    }
+
 }
 
