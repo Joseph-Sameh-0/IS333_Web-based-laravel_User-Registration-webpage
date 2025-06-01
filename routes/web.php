e0e2a66d836d9e333159a20812251be1542fc37c
IsraaXx <saramohamed2315@gmail.com>
2025-06-01 15:24:02 +0300

    route added for Whatsapp

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\WhatsappController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 //    return view('welcome');
     return view('index');
 });
+
+Route::post('/check-whatsapp', [WhatsappController::class, 'check']);
