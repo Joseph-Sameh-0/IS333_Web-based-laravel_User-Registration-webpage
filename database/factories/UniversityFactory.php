45685390c474b2d4353e5950302fab4509ae88b5
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-06-01 04:27:08 +0300

    Add files via upload

---
+++ database/factories/UniversityFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\University>
+ */
+class UniversityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_role' => 'user', // or randomElement(['user', 'admin']) if needed
+            'user_name' => $this->faker->unique()->userName,
+            'full_name' => $this->faker->name,
+            'phone' => $this->faker->phoneNumber,
+            'whatsup_number' => '+20' . $this->faker->numerify('1#########'),
+            'address' => $this->faker->address,
+            'password' => bcrypt('password'), // hashed dummy password
+            'email' => $this->faker->unique()->safeEmail,
+            'student_img' => 'default.jpg', // or path to random image
+        ];
+    }
+}
