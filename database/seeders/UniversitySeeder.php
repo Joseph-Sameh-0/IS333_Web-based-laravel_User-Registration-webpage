1cb36df34bcddcf3699806db98e51955ccb8a0a1
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-06-02 16:42:38 +0300

    Add admin user seeding to UniversitySeeder

--- database/seeders/UniversitySeeder.php
+++ database/seeders/UniversitySeeder.php
@@ -14,5 +14,20 @@ class UniversitySeeder extends Seeder
     public function run(): void
     {
         University::factory()->count(2)->create();  //to create to random rows for fields specified in factory
+
+        // Manually insert admin users
+        University::create([
+            'user_role' => 'admin',
+            'user_name' => 'Joseph',
+            'full_name' => 'Joseph Sameh Fouad',
+            'phone' => '+201118295474',
+            'whatsup_number' => '+201118295474',
+            'address' => 'Admin Address 1',
+            'password' => bcrypt('123456789'), // hashed password
+            'email' => 'joseph.sameh.fouad@gmail.com',
+            'student_img' => 'default.jpg',
+        ]);
+        // Add more admin users as needed
+
     }
 }
