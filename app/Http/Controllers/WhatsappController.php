5744edf21e70ff49c440a0409dcfe86298d02217
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-06-03 01:41:33 +0300

    Add WhatsApp API key management and tests for validation

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -5,17 +5,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class WhatsAppController extends Controller{
-    
-    private $apiKeys = [
-        "a6a34bf089mshda317112704a8adp1cd899jsnea75dc0992e4",
-        "bd4af4fed3msh5cce71bee29321bp143febjsn8784978f97b7",
-        "ac473eab33mshb82011782bcc4aap15c9b3jsn3a63b12de4df",
-        "78718d2a7dmsh7dcfbae259c0f70p1df2dfjsndeea78167fcf",
-        "7934d542a9msh1d255f41af26346p13e040jsn517da03aedf4",
-        "2dcec1e78bmsh0528a4942ec9f47p164759jsn8702f3508936",
-        "af6cb4360amsh9d91a988e4f7df2p11513djsn844b31521070",
-        "f3f7834093mshfff0341a646e910p114b1cjsnb3709cf7e570",
-    ];
+
+    private array $apiKeys;
+
+    public function __construct()
+    {
+        $this->apiKeys = $this->getApiKeys();
+    }
+
+    private function getApiKeys()
+    {
+        $keys = env('WHATSAPP_API_KEYS');
+
+        if (empty($keys)) {
+            return [];
+        }
+
+        return array_map('trim', explode(',', $keys));
+    }
 
     public function check(Request $request)
     {
@@ -24,6 +31,7 @@ class WhatsAppController extends Controller{
         ]);
 
         $phoneNumber = $request->input('phone_number');
+
 
         foreach ($this->apiKeys as $key) {
             try {
---
+++ tests/Http/Controllers/WhatsAppControllerTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use App\Http\Controllers\WhatsAppController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Foundation\Testing\TestCase;
+
+
+class WhatsAppControllerTest extends TestCase
+{
+    public function test_returns_validation_error_if_phone_number_is_missing()
+    {
+        $request = Request::create('/check', 'POST', []);
+
+        $controller = new WhatsAppController();
+
+        try {
+            $controller->check($request);
+
+            // If no exception is thrown, fail the test
+            $this->fail('ValidationException expected');
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            // Get the validator instance
+            $validator = $e->validator;
+
+            // Check that 'phone_number' is among the failed rules
+            $this->assertArrayHasKey('phone_number', $validator->failed());
+
+            // Optional: Check the actual error message
+            $messages = $validator->errors()->messages();
+            $this->assertArrayHasKey('phone_number', $messages);
+        }
+    }
+
+
+    public function test_returns_valid_status_when_api_returns_valid()
+    {
+        // Fake HTTP response
+        Http::fake([
+            '*' => Http::response(['status' => 'valid'], 200),
+        ]);
+
+        $request = Request::create('/check', 'POST', [
+            'phone_number' => '+201205563356'
+        ]);
+
+        $controller = new WhatsAppController();
+
+        $response = $controller->check($request);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(['status' => 'valid'], $response->getData(true));
+    }
+
+
+    public function test_returns_invalid_status_when_api_returns_invalid()
+    {
+        // Fake HTTP response
+        Http::fake([
+            '*' => Http::response(['status' => 'invalid'], 200),
+        ]);
+
+        $request = Request::create('/check', 'POST', [
+            'phone_number' => '+20128347789'
+        ]);
+
+        $controller = new WhatsAppController();
+
+        $response = $controller->check($request);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(['status' => 'invalid'], $response->getData(true));
+    }
+
+
+    public function test_returns_error_status_when_all_apis_fail()
+    {
+        // Fake all API calls to fail
+        Http::fake([
+            '*' => Http::response('Server Error', 500),
+        ]);
+
+        $request = Request::create('/check', 'POST', [
+            'phone_number' => '+20123456789'
+        ]);
+
+        $controller = new WhatsAppController();
+
+        $response = $controller->check($request);
+
+        $this->assertEquals(500, $response->getStatusCode());
+        $this->assertEquals([
+            'status' => 'error',
+            'message' => 'All API keys exhausted or failed.'
+        ], $response->getData(true));
+    }
+}
