c36ec44382e6da3dd8a209c70d60802a03a53622
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-06-03 14:22:42 +0300

    Rename UniversityController to UniversityStudentsController and update routes accordingly

--- app/Http/Controllers/UniversityStudentsController.php
+++ app/Http/Controllers/UniversityStudentsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UniversityUsers;
 use Illuminate\Http\Request;
 
-class UniversityController extends Controller
+class UniversityStudentsController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\UniversityController;
+use App\Http\Controllers\UniversityStudentsController;
 use App\Http\Controllers\WhatsappController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +9,6 @@ Route::get('/', function () {
     return view('users.register');
 });
 
-Route::resource('users', UniversityController::class);
+Route::resource('users', UniversityStudentsController::class);
 
 Route::post('/check-whatsapp', [WhatsappController::class, 'check']);
