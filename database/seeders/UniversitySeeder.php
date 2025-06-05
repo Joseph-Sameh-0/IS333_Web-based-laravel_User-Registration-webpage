0569cd62d13506c7f596d91920702a39b8c87c65
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-06-06 01:34:22 +0300

    Update UniversitySeeder.php

--- database/seeders/UniversitySeeder.php
+++ database/seeders/UniversitySeeder.php
@@ -20,11 +20,23 @@ class UniversitySeeder extends Seeder
             'user_role' => 'admin',
             'user_name' => 'Joseph',
             'full_name' => 'Joseph Sameh Fouad',
-            'phone' => '+201118295474',
+            'phone' => '01118295474',
             'whatsup_number' => '+201118295474',
             'address' => 'Admin Address 1',
             'password' => bcrypt('123456789'), // hashed password
             'email' => 'joseph.sameh.fouad@gmail.com',
+            'student_img' => 'avatar.jpg',
+        ]);
+
+        UniversityUsers::create([
+            'user_role' => 'admin',
+            'user_name' => 'Jonathan',
+            'full_name' => 'Jonathan',
+            'phone' => '01203644940',
+            'whatsup_number' => '+201203644940',
+            'address' => 'Admin Address 2',
+            'password' => bcrypt('Jonathan 123'), // hashed password
+            'email' => '20220100@stud.fci-cu.edu.eg',
             'student_img' => 'avatar.jpg',
         ]);
         // Add more admin users as needed
