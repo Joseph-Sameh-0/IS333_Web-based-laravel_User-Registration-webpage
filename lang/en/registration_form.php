7b20d02fc51b7c796543253b3aab3340c7d1d33a
NourAhmed <147117917+akarin26@users.noreply.github.com>
2025-06-07 16:00:06 +0300

    english registration_form.php

---
+++ lang/en/registration_form.php
@@ -0,0 +1,73 @@
+<?php
+
+return [
+    // Header
+    'FORM' => 'Registration Form',
+    'HOME' => 'Home',
+    'ABOUTUS' => 'About',
+    'CONTACTUS' => 'Contact Us',
+
+    // Form
+    'WELCOME' => 'Welcome to Neovate University,',
+    'SIGNUP' => 'Sign Up Now:',
+
+    'FULLNAME' => 'Full Name:',
+    'NAME_PLACEHOLDER' => 'Enter your Full Name',
+    'NAME_ERR' => 'Minimum 3 characters, only letters and spaces allowed.',
+
+    'USERNAME' => 'Username:',
+    'USERNAME_PLACEHOLDER' => 'Enter your Username',
+    'USERNAME_ERR' => 'Minimum 3 characters, special characters not allowed.',
+
+    'PHONE' => 'Phone:',
+    'PHONE_PLACEHOLDER' => 'Enter your Phone number',
+    'PHONE_ERR' => 'Enter a valid phone number (10-15 digits).',
+
+    'WHATSAPP_NUMBER' => 'WhatsApp Number:',
+    'WHATSAPP_PLACEHOLDER' => 'Enter a valid WhatsApp number',
+    'WHATSAPP_ERR' => 'Enter a valid phone number (10-15 digits).',
+    'CHECK_WHATSAPP' => 'Check WhatsApp',
+
+    'EGYPT' => 'Egypt',
+    'USA' => 'USA',
+    'UK' => 'UK',
+    'UAE' => 'UAE',
+    'INDIA' => 'India',
+
+    'ADDRESS' => 'Address:',
+    'ADDRESS_PLACEHOLDER' => 'Enter your Address',
+    // 'ADDRESS_ERR' => '',
+
+    'EMAIL' => 'Email:',
+    'EMAIL_PLACEHOLDER' => 'OmarAhmed@example.com',
+    'EMAIL_ERR' => 'Email not valid *exemple@yyy.zzz',
+
+    'PASSWORD' => 'Password:',
+    'PASSWORD_PLACEHOLDER' => 'Create a password',
+    'PASSWORD_ERR' => 'Enter valid password *Minimum eight characters, at least one number and one special character:*',
+
+    'CONFIRM_PASSWORD' => 'Confirm Password:',
+    'CONFIRM_PASSWORD_PLACEHOLDER' => 'Confirm your password',
+    'CONFIRM_PASSWORD_ERR' => 'Password does not match',
+
+    'UPLOAD_IMG' => 'Upload Profile Picture:',
+    // 'UPLOAD_PLACEHOLDER' => '',
+    'UPLOAD_ERR' => 'There is no Image Uploaded',
+
+    'HAVE_ACCOUNT' => 'Already have an account?',
+    'LOGIN' => 'Login',
+
+    // 'BROWSE' => 'Browse',
+    'REGISTER' => 'Register',
+
+
+    // Footer
+    'FOOTER_EMAIL' => 'Email: support@site.com',
+    'FOOTER_PHONE' => 'Phone: 0123456789',
+    'LINKS' => 'Quick Links',
+    'POLICY' => 'Privacy Policy',
+    'SERVICE' => 'Terms of Service',
+    'FOLLOWUS' => 'Follow Us',
+    'COPYRIGHT' => 'Neovate University. All rights reserved.',
+
+];
