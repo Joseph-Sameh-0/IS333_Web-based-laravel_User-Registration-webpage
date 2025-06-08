afabeae948535836feca4c4b753880e9d590a6cb
NourAhmed <147117917+akarin26@users.noreply.github.com>
2025-06-08 16:31:44 +0300

    Add files via upload

---
+++ tests/Feature/Controllers/LanguageControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Session;
+use Tests\TestCase;
+
+class LanguageControllerTest extends TestCase
+{
+    public function test_sets_locale_in_session_and_redirects_back()
+    {
+        $previousUrl = '/';
+        $this->get($previousUrl);
+
+        $response = $this->get(route('lang.change', ['lang' => 'ar']));
+
+        $this->assertEquals('ar', Session::get('locale'));
+
+        $response->assertRedirect($previousUrl);
+    }
+}
